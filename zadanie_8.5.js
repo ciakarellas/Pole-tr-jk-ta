f4ea007f0bd8cd6b36e2f9d35558127ca9a0f618
Maciek Wróblewski <Maciek Wrblewski>
2016-12-23 23:39:27 +0100

    Zadanie 8.5

---
+++ zadanie_8.5.js
@@ -0,0 +1,12 @@
+var a = 3;
+var b = 20;
+var value = (a*a) + (2 * a * b) - (b*b);
+
+var wynik = function(a,b)
+if (value > 0) {
+	console.log('wynik dodadni')
+} else if (value < 0) {
+	console.log('wynik ujemny')
+} else {
+	console.log('wynik równy zero')
+}
