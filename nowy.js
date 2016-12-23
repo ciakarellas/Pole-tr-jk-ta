57183b4514facf6e765140b94a4588c47ebdee50
Maciek Wróblewski <Maciek Wrblewski>
2016-12-23 07:14:46 +0100

    skrypt do oplicznai pola trójata

--- nowy.js
+++ nowy.js
@@ -1,1 +1,6 @@
-var a = 0;
+var a = prompt('Chciałbyś obliczyć polę trójkąta prostokątnego?\nNic prostzego podaj mi jego wysokść: ');
+var h = prompt('Teraz podaj długość podstawy: ');
+
+var triangleArea = 0.5*(a*h);
+
+console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
